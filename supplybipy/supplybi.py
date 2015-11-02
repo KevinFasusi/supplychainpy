ec78f1e18e6acd18e8341885a32bd97224a931d6
Kevin Fasusi <Kevin.Fasusi@gmail.com>
2015-11-02 20:35:24 +0000

    eoq added, problem with min variable cost

--- supplybipy/supplybi.py
+++ supplybipy/supplybi.py
@@ -1,6 +1,6 @@
 from supplybipy.build_model import model_orders, analyse_orders_from_file_col, analyse_orders_from_file_row, \
     analyse_orders_abcxyz_from_file
-
+import time
 __author__ = 'kevin'
 
 
@@ -8,17 +8,27 @@ def main():
     r = {'jan': 75, 'feb': 75, 'mar': 75, 'apr': 75, 'may': 75, 'jun': 75, 'jul': 25,
          'aug': 25, 'sep': 25, 'oct': 25, 'nov': 25, 'dec': 25}
 
-    summary = model_orders(r, 'RX983-90', 3, 50.99, 400, 1.28)
-    print(summary)
+    #start_time = time.time()
+    #summary = model_orders(r, 'RX983-90', 3, 50.99, 400, 1.28)
+    #print(summary)
+    #end_time = time.time()
+    #secs = end_time - start_time
+    #print('model_orders took {} seconds to run', secs)
 
     #summary = analyse_orders_from_file_col('test.txt', 'RX9887-90', 4, 45, 400, 1.28)
     #print(summary)
 
-    #big_summary = analyse_orders_from_file_row('test_row.txt', 1.28, 400)
+    start_time = time.time()
+    big_summary = analyse_orders_from_file_row('test_row.txt', 1.28, 400)
     #print(big_summary)
+    end_time = time.time()
+    secs = end_time - start_time
+    print('model_orders took {} seconds to run', secs)
 
-    abc = analyse_orders_abcxyz_from_file('test_row.txt', 1.28, 400)
-
-    print(abc)
-
+    #start_time = time.time()
+    #abc = analyse_orders_abcxyz_from_file('test_row.txt', 1.28, 400)
+    #print(abc)
+    #end_time = time.time()
+    #secs = end_time - start_time
+    #print('model_orders took {} seconds to run', secs)
 if __name__ == '__main__': main()
