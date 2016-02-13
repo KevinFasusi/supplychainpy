1df8c70d3d40527158bbf35678fb4b1080106030
Kevin Fasusi <kevin.fasusi@gmail.com>
2016-02-13 11:17:08 +0000

    fixing docs doctype error and moving docs to one folder

--- docs/conf.py
+++ docs/conf.py
@@ -32,7 +32,7 @@ sys.path.insert(0, os.path.abspath('/home/kevin/Projects/Python/supplychainpy/')
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.autodoc',
+    'sphinx.ext.autodoc','sphinxcontrib.napoleon'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
