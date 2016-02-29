21fce3e7bc3578c439164cc68fbf80bf66738dfb
Kevin Fasusi <Kevin.Fasusi@gmail.com>
2016-02-29 23:38:18 +0000

    updating local repo to switch between machines, changed setup.py version number and dependencies

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='supplychainpy',
-      version='0.0.1',
+      version='0.0.2',
       description='A library for supply chain, operations and manufacturing, analysis, modeling and simulation.',
       url='https://github.com/KevinFasusi/supplychainpy',
       download_url='https://github.com/KevinFasusi/supplychainpy/tarball/0.1',
@@ -10,6 +10,6 @@ setup(name='supplychainpy',
       license='BSD 3',
       packages=find_packages(exclude=['docs', 'tests']),
       test_suite='supplychainpy/tests',
-      install_requires=['MumPy'],
+      install_requires=['NumPy'],
       keywords=['supply chain', 'operations research', 'operations management', 'simulation'],
       )
--- supplychainpy/supplychain.py
+++ supplychainpy/supplychain.py
@@ -1,4 +1,4 @@
-##!/usr/bin/env python3
+#!/usr/bin/env python3
 import os
 from _decimal import Decimal
 
