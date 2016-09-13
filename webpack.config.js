e3b666f2fa9d85c3228123a5c165bd933d756672
Kevin Fasusi <Kevin.Fasusi@gmail.com>
2016-09-13 13:39:20 +0100

    removed webpack config, suchpy_log again from local git sever and updated coveralls config.

--- webpack.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    entry: './supplychainpy/reporting/static/auxiliary-compiled.js',
-    output: {
-        path: './supplychainpy',
-        filename: './supplychainpy/reporting/static/build/bundle.js'
-    },
-    devtool: 'source-map',
-};
