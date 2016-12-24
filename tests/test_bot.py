9b9d8bd0f8572a5950641319b4d3510a8b1f88aa
Kevin Fasusi <Kevin.Fasusi@gmail.com>
2016-12-24 17:46:30 +0000

    Bot Test Startup and Teardown Logic.

--- tests/test_bot.py
+++ tests/test_bot.py
@@ -46,7 +46,7 @@ class TestBot(TestCase):
         greeting2 = self.__dude.chat_machine("hello")[0]
         greeting3 = self.__dude.chat_machine("hello")[0]
         self.assertIn(*greeting3, self.__SALUTATION_RESPONSES)
-        self.assertIn('KR202-244', *self.__dude.chat_machine("Which SKU has the highest reorder level?")[0])
+        self.assertIn('KR202-247', *self.__dude.chat_machine("Which SKU has the highest reorder level?")[0])
         self.assertEqual('<a href="/sku_detail/36">Here you go!</a>', *self.__dude.chat_machine("show KR202-244")[0])
         self.assertIn('SKU KR202-244', *self.__dude.chat_machine("what is the biggest shortage?")[0])
         self.assertIn('KR202-223',*self.__dude.chat_machine("what is the biggest excess?")[0])
