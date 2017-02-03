d44e281b87716705b7f13cb3f844602070bb6998
Kevin Fasusi <Kevin.Fasusi@gmail.com>
2017-02-03 18:23:37 +0000

    Added simulation view to reporting. Added view, pipe for runs.

--- tests/test_bot.py
+++ tests/test_bot.py
@@ -45,7 +45,7 @@ class TestBot(TestCase):
         greeting1 = self.__dude.chat_machine("hello")[0]
         greeting2 = self.__dude.chat_machine("hello")[0]
         self.assertIn(*greeting2, self.__SALUTATION_RESPONSES)
-        self.assertIn('KR202-244', *self.__dude.chat_machine("Which SKU has the highest reorder level?")[0])
+        self.assertIn('KR202-247', *self.__dude.chat_machine("Which SKU has the highest reorder level?")[0])
         self.assertEqual('<a href="/sku_detail/36">Here you go!</a>', *self.__dude.chat_machine("show KR202-244")[0])
         self.assertIn('SKU KR202-244', *self.__dude.chat_machine("what is the biggest shortage?")[0])
         self.assertIn('SKU KR202-223', *self.__dude.chat_machine("what is the biggest excess?")[0])
